3021374ace7f2acbaf22f8e0cd33817e6ebe7bf8
chandramouli1307 <latchireddy13071986@gmail.com>
2017-05-25 15:42:49 +0530

    added rspec gem file

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ApplicationRecord
+
   belongs_to :user
   has_many :goals
   accepts_nested_attributes_for :goals
@@ -12,11 +13,9 @@ class Review < ApplicationRecord
 
   private
     def check_total_weightage
-
       unless self.goals.map(&:weightage).sum == 100
          errors.add(:weightage, "Total weightage must be 100")
        end
      end
-
 
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  
+  it "is valid with valid attributes" do
+    expect(User.new(name: 'Anything', email: "anyemail@gmail.com")).to be_valid
+   end
+
+   it "is not valid without a name" do
+     expect(User.new(name: '', email: "anyemail@gmail.com")).to be_valid
+    end
+
+    it "is not valid without a email" do
+      expect(User.new(name: 'Anything', email: "")).to be_valid
+     end
+end
