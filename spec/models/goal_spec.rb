02b95d4e594b6cd13a926a2cd8952df3bdfcf2c9
chandramouli1307 <latchireddy13071986@gmail.com>
2017-05-30 12:47:45 +0530

    Started writing specs for goal model

---
+++ spec/models/goal_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Goal, type: :model do
+  let(:user) { User.create(name: 'test', email: 'test@gmail.com') }
+  let(:review) { user.reviews.create(name: 'test review') }
+  let(:goal) { review.goals.create(description: 'test description',weightage: 33) }
+
+
+  describe 'validations' do
+   it "is valid with valid attributes" do
+     expect(Goal.new(description: 'test description', weightage: 33)).to be_valid
+    end
+
+    it "is not valid without a description" do
+      expect(Goal.new(description: '', weightage: 33)).to be_valid     
+    end
+
+    it "is not valid without a weightage" do
+      expect(Goal.new(description: 'test description')).to be_valid
+    end
+
+  end
+
+
+end
